26f6e75b3c881c6effe3e83a3692117f34c2a3e0
Fluyd <leafygyt@gmail.com>
2023-08-14 00:15:31 -0400

    FIx

--- src/main/java/dev/fluyd/appwars/utils/config/ConfigUtils.java
+++ src/main/java/dev/fluyd/appwars/utils/config/ConfigUtils.java
@@ -3,7 +3,6 @@ package dev.fluyd.appwars.utils.config;
 import dev.fluyd.appwars.AppWars;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Location;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
@@ -16,30 +15,25 @@ public final class ConfigUtils {
     private final File configFile;
 
     public YamlConfiguration config;
-    private ConfigurationSection settings;
 
     public Location lobbyLocation;
 
     public ConfigUtils() {
         this.configFile = new File(AppWars.INSTANCE.getDataFolder(), "config.yml");
 
-        if (settings == null)
-            return;
-
         this.setValues();
     }
 
     private void setValues() {
         this.config = this.getConfig();
-        this.settings = this.config != null ? this.config.getConfigurationSection("settings") : null;
+
+        if (config == null)
+            throw new RuntimeException("config is null");
 
         /**
          * Config values
          */
-        if (this.settings == null)
-            return;
-
-        this.lobbyLocation = (Location) this.settings.get(ConfigType.LOBBY_LOCATION.getName());
+        this.lobbyLocation = (Location) this.config.get(ConfigType.LOBBY_LOCATION.getName());
     }
 
     public void save() {
